19a7d35f2b3c455fa673211341e5a053a6866ed4
lianghaolin <lianghaolin@morgen-ai.com>
2025-10-15 11:46:36 +0800

    feat(goods): 完成商品微服务

--- service/goods/internal/biz/biz.go
+++ service/goods/internal/biz/biz.go
@@ -1,6 +1,22 @@
 package biz
 
-import "github.com/google/wire"
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"gorm.io/gorm"
+)
 
 // ProviderSet is biz providers.
 var ProviderSet = wire.NewSet(NewGoodsUsecase)
+
+type GoodsUsecase struct {
+	db  *gorm.DB
+	log *log.Helper
+}
+
+func NewGoodsUsecase(db *gorm.DB, logger log.Logger) *GoodsUsecase {
+	return &GoodsUsecase{
+		db:  db,
+		log: log.NewHelper(logger),
+	}
+}
